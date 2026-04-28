7ba3ff03769284d2f8dd977f4c1e5040f76d9624
IT24102379 <IT24102379@my.sliit.lk>
2026-04-28 22:46:07 +0530

    Update backendurl

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import './App.css';
 
-const API_URL = 'http://localhost:5000/api/tasks';
+const API_URL = 'https://wmt-test2.onrender.com/api/tasks';
 
 function App() {
   const [tasks, setTasks] = useState([]);
